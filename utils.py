c571da21a417571bdbd46ed6238970e86e03a7fc
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-08-06 16:24:59 -0500

    pushing working code of project

--- utils.py
+++ utils.py
@@ -11,21 +11,21 @@ nlp = spacy.load("en_core_web_sm")
 stop_words = set(stopwords.words("english"))
 
 def preprocess_text(text):
-    # Step 1: Make all text lowercase
+    # Make all text lowercase
     text = text.lower()
 
-    # Step 2: Remove punctuation
+    # Remove punctuation
     text = text.translate(str.maketrans("", "", string.punctuation))
 
-    # Step 3: Break into words
+    #Break into words
     words = word_tokenize(text)
 
-    # Step 4: Remove stopwords and keep only words (ignore numbers/symbols)
+    #Remove stopwords and keep only words (ignore numbers/symbols)
     words = [word for word in words if word not in stop_words and word.isalpha()]
 
-    # Step 5: Lemmatize (get root words)
+    #Lemmatize (get root words)
     doc = nlp(" ".join(words))
     lemmatized_words = [token.lemma_ for token in doc]
 
-    # Step 6: Return cleaned text
+    #Return cleaned text
     return " ".join(lemmatized_words)
